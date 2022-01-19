68bbc9dd688f1f515314c61063e0b86c529797ec
whyme0 <sparkjetstudiost@gmail.com>
2022-01-19 08:55:57 +0300

    Added docstrings to IPhoneNumber element

--- src/PhoneNumbers/IPhoneNumber.cs
+++ src/PhoneNumbers/IPhoneNumber.cs
@@ -2,10 +2,17 @@
 
 namespace PhoneNumbers
 {
+    /// <summary>
+    /// Represent basic phone number properties.
+    /// </summary>
     public interface IPhoneNumber
     {
+        /// <summary>
+        /// Phone number represented in <c>int</c> type format
+        /// </summary>
         public int PN { get; init; }
-
+        
+        /// <returns>Return human readable phone number from <c>PN</c> field</returns>
         public string ToString();
     }
 }
